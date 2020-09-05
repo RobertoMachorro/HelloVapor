1a15b90f5fcf693a3596d4e76f0ce08efe7dad22
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2020-09-05 13:39:30 -0400

    Sample request handlers (GET and POST). Post accepts and returns JSON data.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,8 +4,37 @@ func routes(_ app: Application) throws {
 	app.get { req in
 		return "It works!"
 	}
-
+	
 	app.get("hello") { req -> String in
 		return "Hello, world!"
 	}
+	
+	app.get("hello", "vapor") { req in
+		return "Hello, Vapor!"
+	}
+	
+	app.get("hello", ":name") { req -> String in
+		guard let name = req.parameters.get("name") else {
+			throw Abort(HTTPResponseStatus.badRequest)
+		}
+		return "Hello, \(name)!"
+	}
+
+	/*
+		curl http://localhost:8080/info \
+			-H "Content-Type: application/json" \
+			-d '{"name":"Tim"}'
+	*/
+	app.post("info") { req -> InfoResponse in
+		let data = try req.content.decode(InfoData.self)
+		return InfoResponse(request: data)
+	}
+}
+
+struct InfoData: Content {
+	let name: String
+}
+
+struct InfoResponse : Content {
+	let request: InfoData
 }
